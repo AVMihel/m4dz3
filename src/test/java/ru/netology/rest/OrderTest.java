0e146ef3975fb3a045948dc41047fd8a5ec3474d
AVMihel <beglec2010@gmail.com>
2025-04-09 09:44:26 +0500

    Complete the second task

--- src/test/java/ru/netology/rest/OrderTest.java
+++ src/test/java/ru/netology/rest/OrderTest.java
@@ -50,6 +50,64 @@ public class OrderTest {
         String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
-}
 
+    //Невалидные данные в поле "Фамилия и имя"
+    @Test
+    void shouldReturnErrorNameField() {
+        List<WebElement> input = driver.findElements(By.cssSelector("input"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ivan");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+73495874356");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'] .input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    //Не заполнено поле "Фамилия и имя"
+    @Test
+    void shouldReturnErrorNameFieldNotFilled() {
+        List<WebElement> input = driver.findElements(By.cssSelector("input"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+73495874356");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'] .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    //Невалидные данные в поле "Мобильный телефон"
+    @Test
+    void shouldReturnErrorPhoneField() {
+        List<WebElement> input = driver.findElements(By.cssSelector("input"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Павел Сергеев");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+7349587435");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    //Не заполнено поле "Мобильный телефо"
+    @Test
+    void shouldReturnErrorPhoneFieldNotFilled() {
+        List<WebElement> input = driver.findElements(By.cssSelector("input"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Павел Сергеев");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    //Не заполнен чек-бокс
+    @Test
+    void shouldNotRegistrationWithoutCheckBox() {
+        List<WebElement> input = driver.findElements(By.cssSelector("input"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Сергей Трофимов");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+77630589265");
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__text")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text.trim());
+    }
+}
 
